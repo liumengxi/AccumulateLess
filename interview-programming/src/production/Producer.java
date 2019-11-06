0ebe6827ffeacd6bac12686887a8ab8f24bb6e5f
liumengxi <316176417@qq.com>
2019-11-06 10:36:24 +0800

    Update Producer.java

--- interview-programming/src/production/Producer.java
+++ interview-programming/src/production/Producer.java
@@ -4,7 +4,6 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * @Description TODO(这里用一句话描述这个类)
- * @Author wb-yzh498839@alibaba-inc.com
  * @date 2019-11-05 17:18
  * @Copyright 2019 Alibaba.com All right reserved.
  */
